e2498178f44083cd5b4e3ab01fd5f1f88dbe6568
Sergei Orlow <hello@orlow.dev>
2021-03-23 00:07:36 +1000

    :lipstick: Make fonts a bit smaller

--- src/typography.js
+++ src/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
 
 export default new Typography({
-	baseFontSize: '18px',
+	baseFontSize: '16px',
 	baseLineHeight: 1.5,
 	blockMarginBottom: 0.8,
 	headerFontFamily: ['Montserrat', 'system'],
