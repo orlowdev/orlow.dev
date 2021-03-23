6f6716afc1368bc90a20120852eba7b7e10e696b
Sergei Orlow <hello@orlow.dev>
2021-03-23 22:26:17 +1000

    :lipstick: Reduce the number of fonts

--- src/typography.js
+++ src/typography.js
@@ -1,27 +1,23 @@
 import Typography from 'typography'
 
 export default new Typography({
-	baseFontSize: '16px',
+	baseFontSize: '18px',
 	baseLineHeight: 1.5,
 	blockMarginBottom: 0.8,
 	headerFontFamily: ['Montserrat', 'system'],
 	headerColor: '#333',
 	headerWeight: '900',
 	bodyWeight: '400',
-	bodyFontFamily: ['Open Sans', 'system'],
+	bodyFontFamily: ['Montserrat', 'system'],
 	bodyColor: '#444',
 	googleFonts: [
 		{
 			name: 'Montserrat',
-			styles: ['900'],
+			styles: ['400', '900'],
 		},
 		{
 			name: 'JetBrains Mono',
-			styles: ['300'],
-		},
-		{
-			name: 'Open Sans',
-			styles: ['100', '400', '700'],
+			styles: ['400'],
 		},
 	],
 })
